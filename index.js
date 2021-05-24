1c2f13608db9aaaaf0b937942eaab35b5ab41437
jass <81079481+jaskaram123@users.noreply.github.com>
2021-05-24 12:52:41 +0530

    Added login and signup forms as well as scrolling back to top

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+var value = document.getElementsByClassName('joinin')
+Array.from(value).forEach(function (element) {
+  element.addEventListener('click', function () {
+    var formtype = element.innerHTML
+    var editform = document.getElementById('typeofform')
+    editform.innerHTML = `${formtype}`
+    document.getElementById('formsign').innerHTML = `${formtype}`
+    document.body.classList.add('openlogin')
+    document.body.style.overflow = 'hidden'
+    document.getElementById('closebtn').addEventListener('click', function () {
+      document.body.classList.remove('openlogin')
+      document.body.style.overflow = 'visible'
+    })
+  })
+})
+document.getElementById('startforfree').addEventListener('click', function () {
+    var editform = document.getElementById('typeofform')
+    editform.innerHTML = 'Sign Up'
+    document.getElementById('formsign').innerHTML ='Sign up'
+    document.body.classList.add('openlogin')
+    document.body.style.overflow = 'hidden'
+    document.getElementById('closebtn').addEventListener('click', function () {
+      document.body.classList.remove('openlogin')
+      document.body.style.overflow = 'visible'
+    })
+})
 
+var learn = document.getElementById('learnmore')
+learn.addEventListener('click', function () {
+    window.scrollTo({
+        top: 603.2000122070312,
+        behavior: 'smooth'
+    })
+})
+document.getElementById('scroll').addEventListener('click', function () {
+    window.scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
